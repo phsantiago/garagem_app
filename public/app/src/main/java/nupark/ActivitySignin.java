a72a5e800a17f7d950d8f6256cec11aa940b80de
Pedro H. Santiago <pedroh_s_@hotmail.com>
2016-07-11 22:39:19 -0300

    initial commit]

---
+++ public/app/src/main/java/nupark/ActivitySignin.java
@@ -0,0 +1,151 @@
+package nupark;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import customfonts.MyEditText;
+import customfonts.MyTextView;
+import nupark.R;
+
+import static com.google.android.gms.common.api.GoogleApiClient.*;
+
+
+public class ActivitySignin extends AppCompatActivity {
+
+    ImageView signinback;
+    View underbar, underbar2;
+    MyTextView signin;
+    MyEditText username;
+    MyEditText password;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+
+        signinback = (ImageView) findViewById(R.id.signinback);
+        underbar = findViewById(R.id.underbar);
+        underbar2 = findViewById(R.id.underbar2);
+        signin = (MyTextView) findViewById(R.id.signin);
+        username = (MyEditText) findViewById(R.id.username);
+        password = (MyEditText) findViewById(R.id.password);
+
+        signinback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(ActivitySignin.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Ion.with(getApplicationContext())
+                        .load("http://192.168.1.37:8080/nupark_api/signin")
+                        .setBodyParameter("usuario", username.getText().toString())
+                        .setBodyParameter("senha", password.getText().toString())
+                        .asString()
+                        .setCallback(new FutureCallback<String>() {
+                            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                            @Override
+                            public void onCompleted(Exception e, String result) {
+
+
+                                JsonObject resobj = new Gson().fromJson(result, JsonObject.class);
+                                System.out.println(result);
+
+                                if (resobj.get("valid").getAsDouble() == 1) {
+                                    Intent it = new Intent(ActivitySignin.this, home.class);
+                                    it.putExtra("nome", resobj.get("nome").toString());
+                                    startActivity(it);
+                                }else{
+                                    Context context = getApplicationContext();
+                                    CharSequence text = "Usuário não encontrado!";
+                                    int duration = Toast.LENGTH_SHORT;
+
+                                    Toast toast = Toast.makeText(context, text, duration);
+                                    toast.show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new Builder(this).addApi(AppIndex.API).build();
+    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client.connect();
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "ActivitySignin Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://nupark.nupark/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.start(client, viewAction);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "ActivitySignin Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://nupark.nupark/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.end(client, viewAction);
+//        client.disconnect();
+//    }
+}
+
+
---
+++ public/app/src/main/java/nupark/ActivitySignup.java
@@ -0,0 +1,79 @@
+package nupark;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+
+public class ActivitySignup extends AppCompatActivity {
+
+  ImageView signupback;
+  customfonts.MyTextView signin;
+  customfonts.MyEditText usuario;
+  customfonts.MyEditText senha;
+  customfonts.MyEditText email;
+  customfonts.MyEditText nome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        signupback = (ImageView)findViewById(R.id.signupback);
+        signin = (customfonts.MyTextView)findViewById(R.id.signin);
+        usuario = (customfonts.MyEditText)findViewById(R.id.usuario);
+        senha = (customfonts.MyEditText)findViewById(R.id.senha);
+        email = (customfonts.MyEditText)findViewById(R.id.email);
+        nome = (customfonts.MyEditText)findViewById(R.id.nome);
+
+        signupback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(ActivitySignup.this, MainActivity.class);
+                startActivity(it);
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Ion.with(getApplicationContext())
+                        .load("http://192.168.1.37:8080/nupark_api/signup")
+                        .setBodyParameter("usuario", usuario.getText().toString())
+                        .setBodyParameter("senha", senha.getText().toString())
+                        .setBodyParameter("email", email.getText().toString())
+                        .setBodyParameter("nome", senha.getText().toString())
+                        .asString()
+                        .setCallback(new FutureCallback<String>() {
+                            @Override
+                            public void onCompleted(Exception e, String result) {
+
+                                if(false){
+                                    Intent it = new Intent(ActivitySignup.this,home.class);
+                                    it.putExtra("nome","value");
+                                    startActivity(it);
+                                }else{
+                                    Context context = getApplicationContext();
+                                    CharSequence text = "Cadastrado!";
+                                    int duration = Toast.LENGTH_SHORT;
+
+                                    Toast toast = Toast.makeText(context, text, duration);
+                                    toast.show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
+    }
+}
